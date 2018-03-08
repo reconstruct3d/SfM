8628121c63804a007b330d36b8827a16008624cc
Muneeb Aadil <imuneebaadil@gmail.com>
2018-03-08 08:35:29 +0500

    added skeleton for incremental sfm

--- src/main.py
+++ src/main.py
@@ -7,10 +7,13 @@ import pdb
 
 from utils import * 
 
-def main(opts): 
-    #Reading two images for reference
-    img1 = cv2.imread('../data/fountain-P11/images/0004.jpg')
-    img2 = cv2.imread('../data/fountain-P11/images/0006.jpg')
+def get3dPtsRef(idx, mask, length): 
+    out = (idx[mask], np.arange(np.sum(mask)))
+    return out 
+
+def getBaselineTriangulation(imgName1,imgName2):
+    img1 = cv2.imread(imgName1)
+    img2 = cv2.imread(imgName2)
 
     #Converting from BGR to RGB format
     img1 = img1[:,:,::-1]
@@ -31,6 +34,11 @@ def main(opts):
                                     param2=opts.fundProb)
     mask = mask.astype(bool).flatten()
 
+    #Digression: Saving pointers from 2D features to 3D points' indices
+    pts3dRef = list()
+    pts3dRef.append(get3dPtsRef(np.array([x.queryIdx for x in matches]), mask, len(kp1)))
+    pts3dRef.append(get3dPtsRef(np.array([x.trainIdx for x in matches]), mask, len(kp2)))
+
     #3. CAMERA POSE ESTIMATION
     K = np.array([[2759.48,0,1520.69],[0,2764.16,1006.81],[0,0,1]]) #hardcoded for now, have to generalize.. 
     E = K.T.dot(F.dot(K))
@@ -49,8 +57,31 @@ def main(opts):
     pts4d = cv2.triangulatePoints(np.eye(3,4),np.hstack((R,t)),img1ptsNorm.T,img2ptsNorm.T)
     pts3d = cv2.convertPointsFromHomogeneous(pts4d.T)[:,0,:]
 
+    return pts3d, pts3dRef
+
+def addViewToReconstruction(imgName, pts3d, pts3dRef): 
+    return 
+
+def addViewsToReconstruction(imgNames, pts3d, pts3dRef): 
+
+    for imgName in imgNames: 
+        addViewToReconstruction(imgName, pts3d, pts3dRef)
+
+    return pts3d, pts3dRef
+
+def main(opts): 
+    #Reading two images for reference
+    imgNames = ['../data/fountain-P11/images/0004.jpg','../data/fountain-P11/images/0005.jpg',
+                '../data/fountain-P11/images/0005.jpg']
+
+    #Initial 2 view SFM
+    _pts3d, _pts3dRef = getBaselineTriangulation(imgNames[0], imgNames[1])
+
+    #Incrementally add more cameras now
+    #pts3d, pts3dRef = addViewsToReconstruction(imgNames[2:], _pts3d, _pts3dRef)
+
     #Finally, saving 3d points in .ply format to view in meshlab software
-    pts2ply(pts3d)
+    pts2ply(_pts3d)
     return 
 
 def SetArguments(parser): 
