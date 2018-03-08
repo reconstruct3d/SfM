5a5e65cb364f68ff5071dc25f9025ba15d431ba4
Muneeb Aadil <imuneebaadil@gmail.com>
2018-03-08 12:01:29 +0500

    added pnp for n camera pose estimation

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np 
 import pickle as pkl 
 import argparse
+from itertools import izip
 
 import pdb
 
@@ -11,7 +12,7 @@ def get3dPtsRef(idx, mask, length):
     out = (idx[mask], np.arange(np.sum(mask)))
     return out 
 
-def getBaselineTriangulation(imgName1,imgName2):
+def getBaselineTriangulation(imgName1,imgName2,K):
     img1 = cv2.imread(imgName1)
     img2 = cv2.imread(imgName2)
 
@@ -23,6 +24,10 @@ def getBaselineTriangulation(imgName1,imgName2):
     surfer=cv2.xfeatures2d.SURF_create()
     kp1, desc1 = surfer.detectAndCompute(img1,None)
     kp2, desc2 = surfer.detectAndCompute(img2,None)
+
+    #Storing keypoitns and descriptors for future reference
+    kps = [kp1, kp2]
+    descs = [desc1, desc2]
 
     matcher = cv2.BFMatcher(crossCheck=True)
     matches = matcher.match(desc1, desc2)
@@ -40,7 +45,6 @@ def getBaselineTriangulation(imgName1,imgName2):
     pts3dRef.append(get3dPtsRef(np.array([x.trainIdx for x in matches]), mask, len(kp2)))
 
     #3. CAMERA POSE ESTIMATION
-    K = np.array([[2759.48,0,1520.69],[0,2764.16,1006.81],[0,0,1]]) #hardcoded for now, have to generalize.. 
     E = K.T.dot(F.dot(K))
     _,R,t,_ = cv2.recoverPose(E,img1pts[mask],img2pts[mask],K)
 
@@ -57,28 +61,78 @@ def getBaselineTriangulation(imgName1,imgName2):
     pts4d = cv2.triangulatePoints(np.eye(3,4),np.hstack((R,t)),img1ptsNorm.T,img2ptsNorm.T)
     pts3d = cv2.convertPointsFromHomogeneous(pts4d.T)[:,0,:]
 
-    return pts3d, pts3dRef
+    return pts3d, pts3dRef, kps, descs
 
-def addViewToReconstruction(imgName, pts3d, pts3dRef): 
-    return 
+def get2D3DMatches(kps, descs, newkp, newdesc, pts3d, pts3dRef): 
+    
+    matcher = cv2.BFMatcher(crossCheck=True)
+    out1, out2 = None, None
+    
+    for i, (_kp, _desc) in enumerate(izip(kps,descs)): 
+        #picking only those descriptors whose 3D points are triangulated
+        kp, desc = np.array(_kp)[pts3dRef[i][0]], np.array(_desc)[pts3dRef[i][0]]
+        
+        matches = matcher.match(desc, newdesc)
+        
+        #Filtering out matched 2D keypoitns from new view
+        imgnidx = np.array([m.trainIdx for m in matches])
+        kpn_ = (np.array(newkp))[imgnidx]
+        imgnpts = np.array([kp.pt for kp in kpn_])
+        
+        #Filtering out already triangulated matched 3D points 
+        imgiidx = np.array([m.queryIdx for m in matches])
+        cloudpts = pts3d[pts3dRef[i][1][imgiidx]]
+        
+        if out1 is None: 
+            out1 = imgnpts
+            out2 = cloudpts
+            
+        else: 
+            out1 = np.concatenate((out1,imgnpts),axis=0)
+            out2 = np.concatenate((out2,cloudpts),axis=0)
+            
+    return imgnpts, cloudpts
 
-def addViewsToReconstruction(imgNames, pts3d, pts3dRef): 
+def getNewCameraPose(imgName, pts3d, pts3dRef, kps, descs, newkp, newdesc, K):
+    imgnpts, cloudpts = get2D3DMatches(kps, descs, newkp, newdesc, pts3d, pts3dRef)
+
+    _, rnvec, tn, mask2 = cv2.solvePnPRansac(cloudpts[:,np.newaxis], imgnpts[:,np.newaxis], K, None,
+                                          confidence=.99,flags=cv2.SOLVEPNP_DLS)
+
+    Rn,_ = cv2.Rodrigues(rnvec)
+
+    return Rn, tn, 
+    
+
+def addViewsToReconstruction(imgNames, pts3d, pts3dRef, kps, descs, K): 
 
     for imgName in imgNames: 
-        addViewToReconstruction(imgName, pts3d, pts3dRef)
 
-    return pts3d, pts3dRef
+        imgn = cv2.imread(imgName)
+        imgn = imgn[:,:,::-1]
+        surfer=cv2.xfeatures2d.SURF_create()
+        newkp, newdesc = surfer.detectAndCompute(imgn,None) 
+
+        Rn, tn = getNewCameraPose(imgName, pts3d, pts3dRef, kps, descs, newkp, newdesc, K)
+
+
+        break 
+
+    return None
 
 def main(opts): 
     #Reading two images for reference
     imgNames = ['../data/fountain-P11/images/0004.jpg','../data/fountain-P11/images/0005.jpg',
                 '../data/fountain-P11/images/0005.jpg']
+    K = np.array([[2759.48,0,1520.69],[0,2764.16,1006.81],[0,0,1]]) #hardcoded for now, have to generalize.. 
 
     #Initial 2 view SFM
-    _pts3d, _pts3dRef = getBaselineTriangulation(imgNames[0], imgNames[1])
+    _pts3d, _pts3dRef, kps, descs = getBaselineTriangulation(imgNames[0], imgNames[1],K)
 
     #Incrementally add more cameras now
-    #pts3d, pts3dRef = addViewsToReconstruction(imgNames[2:], _pts3d, _pts3dRef)
+    addViewsToReconstruction(imgNames[2:], _pts3d, _pts3dRef, kps, descs, K)
+
+    return 
 
     #Finally, saving 3d points in .ply format to view in meshlab software
     pts2ply(_pts3d)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import numpy as np 
 import cv2 
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 def SerializeKeypoints(kp): 
     """Serialize list of keypoint objects so it can be saved using pickle
@@ -123,3 +125,38 @@ def pts2ply(pts,filename='out.ply'):
         
         for pt in pts: 
             f.write('{} {} {} 255 255 255\n'.format(pt[0],pt[1],pt[2]))
+
+def PlotCamera(R,t,ax,scale=.5,depth=.5,faceColor='grey'):
+    C = -t #camera center (in world coordinate system)
+
+    #Generating camera coordinate axes
+    axes = np.zeros((3,6))
+    axes[0,1], axes[1,3],axes[2,5] = 1,1,1
+    
+    #Transforming to world coordinate system 
+    axes = R.T.dot(axes)+C[:,np.newaxis]
+
+    #Plotting axes
+    ax.plot3D(xs=axes[0,:2],ys=axes[1,:2],zs=axes[2,:2],c='r')
+    ax.plot3D(xs=axes[0,2:4],ys=axes[1,2:4],zs=axes[2,2:4],c='g')
+    ax.plot3D(xs=axes[0,4:],ys=axes[1,4:],zs=axes[2,4:],c='b')
+
+    #generating 5 corners of camera polygon 
+    pt1 = np.array([[0,0,0]]).T #camera centre
+    pt2 = np.array([[scale,-scale,depth]]).T #upper right 
+    pt3 = np.array([[scale,scale,depth]]).T #lower right 
+    pt4 = np.array([[-scale,-scale,depth]]).T #upper left
+    pt5 = np.array([[-scale,scale,depth]]).T #lower left
+    pts = np.concatenate((pt1,pt2,pt3,pt4,pt5),axis=-1)
+    
+    #Transforming to world-coordinate system
+    pts = R.T.dot(pts)+C[:,np.newaxis]
+    ax.scatter3D(xs=pts[0,:],ys=pts[1,:],zs=pts[2,:],c='k')
+    
+    #Generating a list of vertices to be connected in polygon
+    verts = [[pts[:,0],pts[:,1],pts[:,2]], [pts[:,0],pts[:,2],pts[:,-1]],
+            [pts[:,0],pts[:,-1],pts[:,-2]],[pts[:,0],pts[:,-2],pts[:,1]]]
+    
+    #Generating a polygon now..
+    ax.add_collection3d(Poly3DCollection(verts, facecolors=faceColor,
+                                         linewidths=1, edgecolors='k', alpha=.25))
