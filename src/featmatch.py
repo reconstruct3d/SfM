16b6415b73b5d63c03a490211b32549531305849
Adeel Ahmad <adeelahmad14@hotmail.com>
2018-05-07 15:31:40 +0500

    Add multiprocessing to featmatch.py

--- src/featmatch.py
+++ src/featmatch.py
@@ -3,8 +3,31 @@ import numpy as np
 import pickle 
 import argparse
 import os 
+import multiprocessing
 
 from utils import * 
+
+def FeatMatchThreaded(opts, img_path, i, img_names):
+    img = cv2.imread(img_path)
+    img_name = img_names[i].split('.')[0]
+    img = img[:,:,::-1]
+
+    feat = getattr(cv2.xfeatures2d, '{}_create'.format(opts.features))()
+    kp, desc = feat.detectAndCompute(img,None)
+
+    kp_ = SerializeKeypoints(kp)
+    
+    with open(os.path.join(feat_out_dir, 'kp_{}.pkl'.format(img_name)),'wb') as out:
+        pickle.dump(kp_, out)
+
+    with open(os.path.join(feat_out_dir, 'desc_{}.pkl'.format(img_name)),'wb') as out:
+        pickle.dump(desc, out)
+
+    if opts.save_results: 
+        raise NotImplementedError
+
+    if (i % opts.print_every) == 0:
+        print('{}/{} features done..'.format(i+1,len(img_paths)))
 
 def FeatMatch(opts, data_files=[]): 
     
@@ -21,27 +44,20 @@ def FeatMatch(opts, data_files=[]):
     if not os.path.exists(feat_out_dir): 
         os.makedirs(feat_out_dir)
     
+    processes = list()
     for i, img_path in enumerate(img_paths): 
-        img = cv2.imread(img_path)
-        img_name = img_names[i].split('.')[0]
-        img = img[:,:,::-1]
+        process = multiprocessing.Process(target=FeatMatchThreaded, args=(opts, img_path, i, img_names))
+        processes.append(process)
 
-        feat = getattr(cv2.xfeatures2d, '{}_create'.format(opts.features))()
-        kp, desc = feat.detectAndCompute(img,None)
+    for process in processes:
+        print("Process starting...")
+        process.start()
+    
+    results = [output.get() for p in processes]
 
-        kp_ = SerializeKeypoints(kp)
-        
-        with open(os.path.join(feat_out_dir, 'kp_{}.pkl'.format(img_name)),'wb') as out:
-            pickle.dump(kp_, out)
-
-        with open(os.path.join(feat_out_dir, 'desc_{}.pkl'.format(img_name)),'wb') as out:
-            pickle.dump(desc, out)
-
-        if opts.save_results: 
-            raise NotImplementedError
-
-        if (i % opts.print_every) == 0:
-            print('{}/{} features done..'.format(i+1,len(img_paths)))
+    for process in processes:
+        print("Waiting for process...")
+        process.join()
 
 def SetArguments(parser): 
 
